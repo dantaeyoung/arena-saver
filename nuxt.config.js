c63b17a9e6df81d9024763a944121859cde7f827
Dan Taeyoung <dan@dantaeyoung.com>
2021-07-30 10:44:08 -0400

    name change

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'arena-gallery',
+    title: 'arena-saver',
     htmlAttrs: {
       lang: 'en'
     },
